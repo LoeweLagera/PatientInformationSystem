39e81fcc0d6d164d64dc0e86fb6d0618bc574038
Charles Jacob <programmercharlesjcb@gmail.com>
2020-08-05 17:29:18 +0800

    Registration Form (Registration of Username and Password Only)

--- app/src/main/java/com/theblockph/patientinformationsystem/RegistrationForm.java
+++ app/src/main/java/com/theblockph/patientinformationsystem/RegistrationForm.java
@@ -1,16 +1,21 @@
 package com.theblockph.patientinformationsystem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 import org.w3c.dom.Text;
@@ -60,12 +65,26 @@ public class RegistrationForm extends AppCompatActivity {
                 if (password.length()<6){
                     setPassword.setError("Password Too Short");
 
-                    //Toast.makeText(RegistrationForm.this, "Password Too Short",Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(RegistrationForm.this, "Password Too Short",Toast.LENGTH_SHORT).show(); !THIS ONE IS A CODE
                 }
-                if(password != confirmPassword){
+                if(!password.equals(confirmPassword)){
                     setPassword.setError("Password Don't Match");
                     setConfirmPassword.setError("Password Don't Match");
                 }
+
+                //Firebase Authentication
+                //CreateUserWithEmailAndPassword used to send the Details in Firebase Cloud "ONLY IN THE AUTHENTICATION TAB"
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            Toast.makeText(RegistrationForm.this, "User Created",Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(RegistrationForm.this, "Error" +task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
             }
         });
