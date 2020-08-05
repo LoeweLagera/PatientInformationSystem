8bd0f35417a6304f57f7fc494cd50e5583e288c6
Charles Jacob <programmercharlesjcb@gmail.com>
2020-08-05 12:48:46 +0800

    Sign Up (onClick Text View) to Registration Form
    Bug - Crashing

--- app/src/main/java/com/theblockph/patientinformationsystem/MainActivity.java
+++ app/src/main/java/com/theblockph/patientinformationsystem/MainActivity.java
@@ -2,16 +2,31 @@ package com.theblockph.patientinformationsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView tvSignUp = findViewById(R.id.txtSignUp);
+
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent signUpIntent  = new Intent(MainActivity.this,RegistrationForm.class);
+                startActivity(signUpIntent);
+            }
+        });
     }
 
 
