ceef6984382c8a4a1424fbe4b6767e0f2a6e3afb
Charles Jacob <programmercharlesjcb@gmail.com>
2020-08-06 17:29:35 +0800

    Clean XML

--- app/src/main/java/com/theblockph/patientinformationsystem/RegistrationForm.java
+++ app/src/main/java/com/theblockph/patientinformationsystem/RegistrationForm.java
@@ -5,9 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.text.TextUtils;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,8 +15,6 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-
-import org.w3c.dom.Text;
 
 import java.util.Objects;
 
@@ -36,9 +32,9 @@ public class RegistrationForm extends AppCompatActivity {
 
         setFirstName = findViewById(R.id.editTxtFirstName);
         setLastName = findViewById(R.id.editTxtLastName);
-        setEmail = findViewById(R.id.editTxtEmail);
-        setPassword = findViewById(R.id.editTxtPassword);
-        setConfirmPassword = findViewById(R.id.editTxtConfirmPassword);
+        setEmail = findViewById(R.id.editTxtEmailRegister);
+        setPassword = findViewById(R.id.editTxtPasswordRegister);
+        setConfirmPassword = findViewById(R.id.editTxtConfirmPasswordRegister);
 
         RegisterBtn = findViewById(R.id.btnRegister);
 
