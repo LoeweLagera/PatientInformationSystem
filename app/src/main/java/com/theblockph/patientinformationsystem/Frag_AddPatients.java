ed563a6267ee300b10ab7ee11a79fa89d29b08eb
LoeweLagera <marttloewe@gmail.com>
2020-08-06 20:10:09 +0800

    UPDATE: Created add patient fields
    incomplete fields

--- app/src/main/java/com/theblockph/patientinformationsystem/Frag_AddPatients.java
+++ app/src/main/java/com/theblockph/patientinformationsystem/Frag_AddPatients.java
@@ -6,15 +6,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class Frag_AddPatients extends AppCompatActivity {
+public class Frag_AddPatients extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frag__add_patients);
+
+        Spinner spinner = findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.numbers, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 
         //Initialize and Assign Value
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -26,33 +40,44 @@ public class Frag_AddPatients extends AppCompatActivity {
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
+                switch (item.getItemId()) {
                     case R.id.dashboard:
                         startActivity(new Intent(getApplicationContext()
-                                ,Frag_Dashboard.class));
-                        overridePendingTransition(0,0);
+                                , Frag_Dashboard.class));
+                        overridePendingTransition(0, 0);
                         return true;
                     case R.id.patients:
                         startActivity(new Intent(getApplicationContext()
-                                ,Frag_Patients.class));
-                        overridePendingTransition(0,0);
+                                , Frag_Patients.class));
+                        overridePendingTransition(0, 0);
                         return true;
                     case R.id.add_patients:
 
                         return true;
                     case R.id.lab_results:
                         startActivity(new Intent(getApplicationContext()
-                                ,Frag_LaboratoryResults.class));
-                        overridePendingTransition(0,0);
+                                , Frag_LaboratoryResults.class));
+                        overridePendingTransition(0, 0);
                         return true;
                     case R.id.prescriptions:
                         startActivity(new Intent(getApplicationContext()
-                                ,Frag_Prescriptions.class));
-                        overridePendingTransition(0,0);
+                                , Frag_Prescriptions.class));
+                        overridePendingTransition(0, 0);
                         return true;
                 }
                 return false;
             }
         });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
