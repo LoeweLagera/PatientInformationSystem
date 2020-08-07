4051791eab3c52c6a266095d8787a852fd94ed8f
LoeweLagera <marttloewe@gmail.com>
2020-08-07 17:00:40 +0800

    ADDED: can now Register Patients to Database. Redirect After Registration
    FIXED:

--- app/src/main/java/com/theblockph/patientinformationsystem/Frag_AddPatients.java
+++ app/src/main/java/com/theblockph/patientinformationsystem/Frag_AddPatients.java
@@ -1,37 +1,132 @@
 package com.theblockph.patientinformationsystem;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Frag_AddPatients extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    //Insert Data to Firebase - find: Connect1
+    EditText reg_patientfname, reg_patientmname, reg_patientlname;
+    Spinner reg_gender, reg_marital;
+    EditText reg_address, reg_religion, reg_occupation;
+    EditText reg_age, reg_contact1, reg_contact2, reg_email;
+
+    Button btn_register;
+    Table_Patient table_patient;
+    DatabaseReference reff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frag__add_patients);
 
+        //=============================INSERT DATA TO DATABASE==================================
+
+        //Insert Data to Firebase - name: Connect1
+
+        //                      -----name_of_Activity------
+        setContentView(R.layout.activity_frag__add_patients);
+
+        //======================DATA TO BE INSERTED TO FIREBASE============================
+            //                       -----Name_of_TextViews-----
+        reg_patientfname=(EditText) findViewById(R.id.reg_patientfname);
+        reg_patientmname=(EditText) findViewById(R.id.reg_patientmname);
+        reg_patientlname=(EditText) findViewById(R.id.reg_patientlname);
+
+        //                    -----Spinners-----
+        reg_gender=(Spinner) findViewById(R.id.reg_gender);
+        reg_marital=(Spinner) findViewById(R.id.reg_marital);
+
+
+        reg_address=(EditText) findViewById(R.id.reg_address);
+        reg_religion=(EditText) findViewById(R.id.reg_religion);
+
+            //        ---AGE---Occupation---CELLPHONE---TELEPHONE---
+        reg_age=(EditText) findViewById(R.id.reg_age);
+        reg_occupation=(EditText) findViewById(R.id.reg_occupation);
+        reg_contact1=(EditText) findViewById(R.id.reg_contact1);
+        reg_contact2=(EditText) findViewById(R.id.reg_contact2);
+        reg_email=(EditText) findViewById(R.id.reg_email);
+
+        //                -----Button_to_insert_data_to_Firebase-----
+        btn_register=(Button) findViewById(R.id.btn_register);
+
+        //    Declared above      -----Patient.java-----
+        table_patient=new Table_Patient();
+
+        //                      -----Firebase_Reference-----
+        reff= FirebaseDatabase.getInstance().getReference().child("Patients");
+
+        //OnClickListener
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int age=Integer.parseInt(reg_age.getText().toString().trim());
+                Long cellphone=Long.parseLong(reg_contact1.getText().toString().trim());
+                Long telephone=Long.parseLong(reg_contact2.getText().toString().trim());
+                // For integers and LONG convert to string first ^^^ CHECK METHOD HERE ^^^
+
+                table_patient.setFname(reg_patientfname.getText().toString().trim());
+                table_patient.setMname(reg_patientmname.getText().toString().trim());
+                table_patient.setLname(reg_patientlname.getText().toString().trim());
+                table_patient.setGender(reg_gender.getSelectedItem().toString().trim());
+                table_patient.setMarital(reg_marital.getSelectedItem().toString().trim());
+
+                table_patient.setAddress(reg_address.getText().toString().trim());
+                table_patient.setReligion(reg_religion.getText().toString().trim());
+
+                table_patient.setAge(age);
+                table_patient.setCellphone(cellphone);
+                table_patient.setTelephone(telephone);
+
+                table_patient.setOccupation(reg_occupation.getText().toString().trim());
+                table_patient.setEmail(reg_email.getText().toString().trim());
+
+
+                reff.push().setValue(table_patient);
+
+                //redirect to Patients Tab
+                startActivity(new Intent(getApplicationContext()
+                        , Frag_Patients.class));
+                overridePendingTransition(0, 0);
+
+                Toast.makeText(Frag_AddPatients.this, "Patient Registered!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^END OF INSERT DATA^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
         //Spinner for Marital Status
-        Spinner spinner = findViewById(R.id.spinner1);
+        Spinner spinner = findViewById(R.id.reg_marital);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.numbers, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
 
-        //spinner for Religion
+        //Spinner for Gender
+        Spinner spinner_Gender = findViewById(R.id.reg_gender);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,R.array.Gender, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_Gender.setAdapter(adapter2);
+        spinner_Gender.setOnItemSelectedListener(this);
 
         //Initialize and Assign Value
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -72,9 +167,6 @@ public class Frag_AddPatients extends AppCompatActivity implements AdapterView.O
             }
         });
 
-        //=============================INSERT DATA TO DATABASE==================================
-
-
     }
 
     @Override
@@ -88,4 +180,9 @@ public class Frag_AddPatients extends AppCompatActivity implements AdapterView.O
 
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+    }
 }
---
+++ app/src/main/java/com/theblockph/patientinformationsystem/Table_Patient.java
@@ -0,0 +1,111 @@
+package com.theblockph.patientinformationsystem;
+
+public class Table_Patient {
+
+    private String fname, mname, lname;
+    private String gender, marital, address;
+    private String religion, occupation;
+    private Integer age;
+    private Long cellphone, telephone;
+    private String email;
+
+
+    public Table_Patient() {
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getMname() {
+        return mname;
+    }
+
+    public void setMname(String mname) {
+        this.mname = mname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getMarital() {
+        return marital;
+    }
+
+    public void setMarital(String marital) {
+        this.marital = marital;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getReligion() {
+        return religion;
+    }
+
+    public void setReligion(String religion) {
+        this.religion = religion;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Long getCellphone() {
+        return cellphone;
+    }
+
+    public void setCellphone(Long cellphone) {
+        this.cellphone = cellphone;
+    }
+
+    public Long getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(Long telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
